d6b1bc7f1e8cdc29c455eda1f75d6bfc6802f956
jmscaustin1991 <167023595+jmscaustin1991@users.noreply.github.com>
2025-08-10 17:33:11 -0500

    Create nanoid.ts

---
+++ src/features/util/nanoid.ts
@@ -0,0 +1,6 @@
+export function nanoid(size = 12) {
+  const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  let id = ''
+  for (let i = 0; i < size; i++) id += chars[(Math.random()*chars.length)|0]
+  return id
+}
